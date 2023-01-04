aeed2622a7be2ec1e3b409fbba493a3125423678
EzequielGPerez <e.giovanni9292@gmail.com>
2023-01-04 15:42:10 -0800

    Revisions complete

--- src/App.js
+++ src/App.js
@@ -14,14 +14,13 @@ import mockCats from "./mockCats"
 
 const App = () => {
   const [cats, setCats] = useState(mockCats)
-  // console.log(mockCats)
 
   return (
 <>
 <Header />
 <Routes>
   <Route path="/" element={<Home />} />
-  <Route path="/catindex" element={<CatIndex cats={mockCats} />} />
+  <Route path="/catindex" element={<CatIndex cats={cats} />} />
   <Route path="/catshow/:id" element={<CatShow cats={cats} />} />
   <Route path="/catnew" element={<CatNew />} />
   <Route path="/catedit" element={<CatEdit />} />
--- src/pages/CatShow.test.js
+++ src/pages/CatShow.test.js
@@ -1,7 +1,8 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import CatShow from "./CatShow"
 import { MemoryRouter, Routes, Route } from "react-router-dom"
 import mockCats from "../mockCats"
+import { Card } from "reactstrap"
 
 
 describe("<CatShow />", () => {
@@ -16,6 +17,46 @@ describe("<CatShow />", () => {
     </MemoryRouter>,
     div)
   })
+
+  it("renders cat cards", () => {
+    const div = document.createElement("div")
+    render(
+      <MemoryRouter initialEntries={["/catshow/1"]}>
+      <Routes>
+         <Route path="catshow/:id" element={<Card />} />
+        </Routes>
+       </MemoryRouter>,
+       div)
+
+       const CatShow = ({ cats }) => {
+        const { id } = useParams()
+        let currentCat = cats?.find((cat) => cat.id === +id)
+      
+      
+      
+        return (
+          <>
+            <Card
+              style={{
+                width: "14rem"
+              }}
+              key={id}
+            >
+              <img alt={`profile of a currentCat named ${currentCat.name}`} src={currentCat.image} />
+              <CardBody>
+               <p><CardTitle tag="h5">{currentCat.name}</CardTitle></p> 
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  <p>Age: {currentCat.age}</p>
+                  <p>Enjoys: {currentCat.enjoys}</p>
+                </CardSubtitle>
+      
+              </CardBody>
+            </Card>
+          </>
+        )
+      }
+  })
+
 })
 
 
