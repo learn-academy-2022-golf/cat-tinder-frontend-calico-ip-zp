3b7e3e34911b06fdfc1e2e49286712efee0cb4cc
Mannyplg <pettigrew.i@gmail.com>
2023-01-05 11:36:09 -0800

    changes made

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -27,66 +27,66 @@ const handleSubmit = () => {
   return (
 <div>
   <p>Create a new cat</p>
-<Form>
-  <Row>
-    <Col md={6}>
-      <FormGroup>
-        <Label for="name">
-          Name
-        </Label>
-        <Input
-          name="name"
-          placeholder="Please enter name."
-          type="text"
-          onChange={handleChange}
-          value={newCat.name}
-        />
-      </FormGroup>
-    </Col>
-    <Col md={6}>
-      <FormGroup>
-        <Label for="age">
-          Age
-        </Label>
-        <Input
-          name="age"
-          placeholder="Please enter age."
-          type="number"
-          onChange={handleChange}
-          value={newCat.age}
-        />
-      </FormGroup>
-    </Col>
-  </Row>
-  <FormGroup>
-    <Label for="enjoys">
-      Enjoys
-    </Label>
-    <Input
-      name="enjoys"
-      placeholder="Please enter your likes..."
-      type="text"
-      onChange={handleChange}
-      value={newCat.enjoys}
-    />
-  </FormGroup>
-  <FormGroup>
-    <Label for="image">
-      Image
-    </Label>
-    <Input
-      name="image"
-      placeholder="Share image."
-      type="url"
-      onChange={handleChange}
-      value={newCat.url}
-    />
-  </FormGroup>
-</Form>
+  <Form>
+    <Row>
+      <Col md={6}>
+        <FormGroup>
+          <Label for="name">
+            Name
+          </Label>
+          <Input
+            name="name"
+            placeholder="Please enter name."
+            type="text"
+            onChange={handleChange}
+            value={newCat.name}
+          />
+        </FormGroup>
+      </Col>
+      <Col md={6}>
+        <FormGroup>
+          <Label for="age">
+            Age
+          </Label>
+          <Input
+            name="age"
+            placeholder="Please enter age."
+            type="number"
+            onChange={handleChange}
+            value={newCat.age}
+          />
+        </FormGroup>
+      </Col>
+    </Row>
+    <FormGroup>
+      <Label for="enjoys">
+        Enjoys
+      </Label>
+      <Input
+        name="enjoys"
+        placeholder="Please enter your likes..."
+        type="text"
+        onChange={handleChange}
+        value={newCat.enjoys}
+      />
+    </FormGroup>
+    <FormGroup>
+      <Label for="image">
+        Image
+      </Label>
+      <Input
+        name="image"
+        placeholder="Share image."
+        type="url"
+        onChange={handleChange}
+        value={newCat.url}
+      />
+    </FormGroup>
+  </Form>
 
-<Button onClick={handleSubmit} name="submit">
-Submit Cat
-</Button> 
+  <Button onClick={handleSubmit} name="submit">
+  Submit Cat
+  </Button> 
 </div>
   )
 }
